c4c1d1a2d43fb6a92293a96e70b06993fd4acdbb
vladikoff <vlad.filippov@gmail.com>
2017-07-31 10:36:33 -0400

    Add install step for lib

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,6 +3,7 @@
 set -e
 
 cp node_modules/testpilot-ga/dist/index.js src/vendor/testpilot-ga.js
+cp node_modules/fxa-crypto-relier/dist/FxaCrypto.relier.js src/vendor/FxaCrypto.relier.js
 cp node_modules/quill/dist/quill.min.js src/sidebar/vendor/quill.js
 cp node_modules/quill/dist/quill.snow.css src/sidebar/vendor
 
